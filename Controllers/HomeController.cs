2a2a0b08a7c870a2ad528683147a54c0927a9ac7
Angel Samaniego <171G0634@rcarbonifera.tecnm.mx>
2020-09-25 16:21:15 -0500

    Subiendo archivo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Unidad2Actividad1.Models;
+
+namespace Unidad2Actividad1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sumar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Resultado(Ejemplo em)
+        {
+            return View(em);
+        }
+
+    }
+}
---
+++ Models/Ejemplo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Unidad2Actividad1.Models
+{
+    public class Ejemplo
+    {
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+        private int res;
+
+        public int Res
+        {
+            get { return res = Numero1 + Numero2; }
+            set { res = value; }
+        }
+
+    }
+}
